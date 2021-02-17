b0d027cad226b10d88a30353436ea927c6ff2957
Antonio d'Errico <tony.derrico@outlook.it>
2021-02-17 15:31:20 +0100

    Update phaseOne_EN6892018.R

--- R/phaseOne_EN6892018.R
+++ R/phaseOne_EN6892018.R
@@ -1,6 +1,6 @@
 #'Phase 1, EN689 2018  - testing compliance for 3 measurements
 #'
-#'Potential compliance, uncertain compliance or non-compliance of a SEG can be
+#'Compliance, uncertain compliance or non-compliance of a SEG can be
 #'evaluated for three measurements. Compliance is achieved if all of
 #'the measurements are below or equal to 0.1xOEL. There is Uncertain Compliance if
 #'at least a measurement is greater than 0.1xOEL but below the OEL.
@@ -16,7 +16,7 @@
 
 #'Phase 1, EN689 2018  - testing compliance for 4 measurements
 #'
-#'Potential compliance, uncertain compliance or non-compliance of a SEG can be
+#'Compliance, uncertain compliance or non-compliance of a SEG can be
 #'evaluated for four measurements. Compliance is achieved if all of the measurements
 #'are below or equal to 0.15xOEL. There is Uncertain Compliance if at least a
 #'measurement is greater than 0.15xOEL but below the OEL. There is Non-Compliance
@@ -31,7 +31,7 @@ phase1EN2018_k4 <- function(x, OEL) {
 
 #'Phase 1, EN689 2018  - testing compliance for 5 measurements
 #'
-#'Potential compliance, uncertain compliance or non-compliance of a SEG can be evaluated for five measurements. Compliance is achieved if all of the measurements are below or equal to 0.2xOEL. There is Uncertain Compliance if at least a measurement is greater than 0.2xOEL but below the OEL. There is Non-Compliance if at least a measurement is greater than OEL.
+#'Compliance, uncertain compliance or non-compliance of a SEG can be evaluated for five measurements. Compliance is achieved if all of the measurements are below or equal to 0.2xOEL. There is Uncertain Compliance if at least a measurement is greater than 0.2xOEL but below the OEL. There is Non-Compliance if at least a measurement is greater than OEL.
 #' @param x Five measurements (numeric)
 #' @param OEL Establish the OEL (numeric)
 #' @return Compliance ("C"), Non-Compliance ("NC") or Uncertainty of Compliance ("UC")

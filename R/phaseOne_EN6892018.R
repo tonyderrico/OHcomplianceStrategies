e1b7b15ec949f6f6e6e0ce2ea9683d299ee134e5
Antonio D'Errico <tony.derrico@outlook.it>
2021-03-14 14:39:25 +0100

    Update phaseOne_EN6892018.R

--- R/phaseOne_EN6892018.R
+++ R/phaseOne_EN6892018.R
@@ -5,13 +5,13 @@
 #'the measurements are below or equal to 0.1xOEL. There is Uncertain Compliance if
 #'at least a measurement is greater than 0.1xOEL but below the OEL.
 #'There is Non-Compliance if at least a measurement is greater than OEL.
-#' @param x Three measurements (numeric)
-#' @param OEL Establish the OEL (numeric)
+#' @param samples Three measurements
+#' @param OEL Occupational Exposure Limit of the agent
 #' @return Compliance ("C"), Non-Compliance ("NC") or Uncertainty of Compliance ("UC")
 #' @export
 
   phase1EN2018_k3 <- function(samples, OEL) {
-    ifelse(any(x[1:3] > OEL), "NC", ifelse(any(x[1:3] >= 0.1*OEL), "UC","C"))
+    ifelse(any(samples[1:3] > OEL), "NC", ifelse(any(samples[1:3] >= 0.1*OEL), "UC","C"))
   }
 
 #'Phase 1, EN689 2018  - testing compliance for 4 measurements
@@ -21,21 +21,21 @@
 #'are below or equal to 0.15xOEL. There is Uncertain Compliance if at least a
 #'measurement is greater than 0.15xOEL but below the OEL. There is Non-Compliance
 #'if at least a measurement is greater than OEL.
-#' @param x Four measurements (numeric)
-#' @param OEL Establish the OEL (numeric)
+#' @param x Four measurements 
+#' @param OEL Occupational Exposure Limit of the agent
 #' @return Compliance ("C"), Non-Compliance ("NC") or Uncertainty of Compliance ("UC")
 #' @export
 
 phase1EN2018_k4 <- function(samples, OEL) {
-  ifelse(any(x[1:4] > OEL), "NC", ifelse(any(x[1:4] >= 0.15*OEL), "UC","C"))}
+  ifelse(any(samples[1:4] > OEL), "NC", ifelse(any(samples[1:4] >= 0.15*OEL), "UC","C"))}
 
 #'Phase 1, EN689 2018  - testing compliance for 5 measurements
 #'
 #'Compliance, uncertain compliance or non-compliance of a SEG can be evaluated for five measurements. Compliance is achieved if all of the measurements are below or equal to 0.2xOEL. There is Uncertain Compliance if at least a measurement is greater than 0.2xOEL but below the OEL. There is Non-Compliance if at least a measurement is greater than OEL.
-#' @param x Five measurements (numeric)
-#' @param OEL Establish the OEL (numeric)
+#' @param samples Five measurements 
+#' @param OEL Occupational Exposure Limit of the agent
 #' @return Compliance ("C"), Non-Compliance ("NC") or Uncertainty of Compliance ("UC")
 #' @export
 
-phase1EN2018_k5 <- function(x, OEL) {
-  ifelse(any(x[1:5] > OEL), "NC", ifelse(any(x[1:5] >= 0.2*OEL), "UC","C"))}
+phase1EN2018_k5 <- function(samples, OEL) {
+  ifelse(any(samples[1:5] > OEL), "NC", ifelse(any(samples[1:5] >= 0.2*OEL), "UC","C"))}

518d92b8cac685b07586356b4b45ee0d2d6c9144
Antonio D'Errico <tony.derrico@outlook.it>
2021-03-14 14:43:49 +0100

    Update phaseTwo_EN6892018.R

--- R/phaseTwo_EN6892018.R
+++ R/phaseTwo_EN6892018.R
@@ -5,21 +5,22 @@
 #' there is Compliance, differently, if the U value is lower, there is Non-Compliance.
 #' The U-thresholds integrated in the function are for a maximum of 15 measurements per SEG.
 #'
-#' @param x measurements of the SEG under assessment from 6 to 15
+#' @param samples measurements of the SEG under assessment from 6 to 15
+#' @param Occupational Exposure Limit of the agent
 #' @return U value > ("TRUE") or < ("FALSE)  U thresholds
 #' @export
 
-phase2_Uvalue <- function(x, OEL){
-  U <- (log(OEL) - log(geomean(x))) / log(geosd(x))
-  if(length(x) == 6)
+phase2_Uvalue <- function(samples, OEL){
+  U <- (log(OEL) - log(geomean(samples))) / log(geosd(samples))
+  if(length(samples) == 6)
   {U > 2.187}
-  else if(length(x) == 7)
+  else if(length(samples) == 7)
   {U > 2.120}
-  else if(length(x) == 8)
+  else if(length(samples) == 8)
   {U > 2.072}
-  else if(length(x) == 9)
+  else if(length(samples) == 9)
   {U > 2.035}
-  else if(length(x) == 10)
+  else if(length(samples) == 10)
   {U > 2.005}
   }
 
@@ -28,12 +29,13 @@ phase2_Uvalue <- function(x, OEL){
 #' The test is based on the comparison of the UTL having 95% Confidence Interval with 70% Confidence
 #' Level with the OEL. If the UTL is greater than OEL, there is exceedance and so Non-Compliance.
 #' Contrarly, if the UTL is lower than OEL, the probability of exceedance is acceptable, so there is Compliance.
-#' @param x at least 6 measurements of the SEG under assessment
+#' @param samples at least 6 measurements of the SEG under assessment
+#' @param OEL Occupational Exposure Limit of the agent
 #' @return UTL > OEL ("TRUE") or UTL < OEL ("FALSE")
 #' @export
 
-phase2_UTL <- function(x, OEL) {
-  TL <- normtol.int(log(x), alpha = 0.3, P = 0.95, side = 1)
+phase2_UTL <- function(samples, OEL) {
+  TL <- normtol.int(log(samples), alpha = 0.3, P = 0.95, side = 1)
   UTL <- TL$`1-sided.upper`
   ifelse(exp(UTL) > OEL, "TRUE", "FALSE")
 }

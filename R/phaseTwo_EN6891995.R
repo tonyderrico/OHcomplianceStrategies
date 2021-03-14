fb43f808e3e3bbb5bf49b01b75fcc05708b85c0c
Antonio D'Errico <tony.derrico@outlook.it>
2021-03-14 14:28:03 +0100

    Update phaseTwo_EN6891995.R

--- R/phaseTwo_EN6891995.R
+++ R/phaseTwo_EN6891995.R
@@ -3,13 +3,13 @@
 #' The normal distribution of the measurements is operated, and 99.9th percentile and 95th percentile values are observed to assess the possible exceedance of the OEL.
 #' Compliance or "Green Area" is achieved if the value of the 99,9 the percentile is lower than the OEL, Uncertain Compliance or "Orange Area" is achieved if
 #' the value of the 95th percentile is lower than the OEL. Non Compliance or "Red Area" is achieved if the OEL is greater than the value of the 95th percentile.
-#' @param x measurements of the SEG under assessment
+#' @param samples measurements of the SEG under assessment
 #' @return Green Area, Orange Area or Red Area
 #' @export
 
-phase2EN689.1995 <- function(x, OEL) {
-  QN <- qnorm(0.999, mean(x), sd(x))
-  QN1 <- qnorm(0.95, mean(x), sd(x))
+phase2EN689.1995 <- function(samples, OEL) {
+  QN <- qnorm(0.999, mean(samples), sd(samples))
+  QN1 <- qnorm(0.95, mean(samples), sd(samples))
   if(QN < OEL)
   {print("Green Area")}
   else if(QN1 < OEL)

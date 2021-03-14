4816b70434876418537d6399f33c6dd01c82dd6c
Antonio D'Errico <tony.derrico@outlook.it>
2021-03-14 15:18:04 +0100

    Update OverExp_Exc.R

--- R/OverExp_Exc.R
+++ R/OverExp_Exc.R
@@ -1,17 +1,17 @@
 #'OverExposure
 #'
 #' OverExposure Calculation
-#' @param samples measurements and repeats of the SEG under assessment
+#' @param seg measurements and repeats of the SEG under assessment
 #' @param workers workers codes/names/ID
-#' @param agent concentration of the agent
+#' @param samples samples concentrations of the exposed workers
 #' @param OEL Occupational Exposure Limit of the agent
 #' @return % of OverExposure
 #' @export
 
-OverExp <- function(samples, workers, agent, OEL) {
-  M <- xA %>% group_by(workers) %>% summarise(mean = mean(agent))
+OverExp <- function(seg, workers, samples, OEL) {
+  M <- xA %>% group_by(workers) %>% summarise(mean = mean(samples))
   M1 <- mean(M$mean)
-  xA <- lmer(agent~1 + ( 1| workers), data = samples)
+  xA <- lmer(samples~1 + ( 1| workers), data = seg)
   VCrandom <- VarCorr(xA)
   vv <- as.data.frame(VCrandom)
   wwsd <- sqrt(vv$vcov[2])
@@ -23,14 +23,14 @@ OverExp <- function(samples, workers, agent, OEL) {
 #'Exceedance
 #'
 #' Probability of the OEL exceedance of the SEG considered
-#' @param samples measurements under assessment
+#' @param seg measurements and repeats of the SEG under assessment
 #' @param OEL Occupational Exposure Limit of the agent 
 #' @return % of Exceedance
 #' @export
 
-  Exceedance <- function(samples,OEL) {
-    GM <- geomean(samples)
-    GSD <- geosd(samples)
+  Exceedance <- function(seg,OEL) {
+    GM <- geomean(seg)
+    GSD <- geosd(seg)
     Exc <- (log(OEL) - log(GM)) / log(GSD)
     1 - pnorm(Exc)
   }
